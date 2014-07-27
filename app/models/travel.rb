319ff4512171b34808034efc6994b708c95e2fe1
Carlos Aguilar <caguilar@dwdandsolutions.com>
2014-07-27 10:21:09 -0600

    Changing method to syncronize with API

--- app/models/travel.rb
+++ app/models/travel.rb
@@ -1,6 +1,11 @@
 class Travel < ActiveRecord::Base
-  def self.sync_with_api
+  def self.sync_with_api(opts={})
     url = 'http://api.gobiernoabierto.gob.sv/institution_travels?per_page=100&page=2&q[start_date_gteq]=2014-01-01'
+    params = {
+        'per_page' => 100,
+        'q[start_date_gteq]' => '2014-01-01',
+        'page' => opts.fetch(:page) {1}
+    }
     request = Typhoeus::Request.new(url, headers: {Authorization: 'Token token="36bc11762f97f155729497f7099d76c4"'})
 
     request.on_complete do |response|
