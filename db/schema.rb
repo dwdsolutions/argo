419c1d0b196386e6ee8ee19dd218c042e4c65c6e
hkadejo <hkadejo@gmail.com>
2014-07-27 11:05:18 -0600

    Rake tasks for populate PriceComparation table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140727003216) do
+ActiveRecord::Schema.define(version: 20140727150943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "price_comparations", force: true do |t|
+    t.string   "name"
+    t.decimal  "unit_cost",          precision: 10, scale: 2
+    t.string   "source_information"
+    t.string   "category"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "travels", force: true do |t|
     t.string   "name"
