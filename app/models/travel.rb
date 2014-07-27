24ec587567e81e26475c01100a129d0f11c6e0b5
Carlos Aguilar <caguilar@dwdandsolutions.com>
2014-07-27 10:03:48 -0600

    Adding method to sync with API

--- app/models/travel.rb
+++ app/models/travel.rb
@@ -1,2 +1,15 @@
 class Travel < ActiveRecord::Base
+  def self.sync_with_api
+    url = 'http://api.gobiernoabierto.gob.sv/institution_travels?per_page=100&page=2&q[start_date_gteq]=2014-01-01'
+    request = Typhoeus::Request.new(url, headers: {Authorization: 'Token token="36bc11762f97f155729497f7099d76c4"'})
+
+    request.on_complete do |response|
+      if response.code == 200
+        @response = JSON.parse(response.response_body)
+      end
+    end
+
+    request.run
+    puts "Retornados: #{@response.length}"
+  end
 end
