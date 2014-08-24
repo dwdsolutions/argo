81c5e6ade942614db6c820176e2c0f6e6a85063c
Carlos Aguilar <caguilar@dwdandsolutions.com>
2014-08-24 17:19:49 -0600

    Change the namespace from Api to API

--- app/controllers/api/v1/price_comparation_controller.rb
+++ app/controllers/api/v1/price_comparation_controller.rb
@@ -1,24 +1,24 @@
-module Api
-    module V1
-	class PriceComparationController < ApplicationController
-	
-	  def compare
-                @travel = Travel.where(:id => params[:travel_id]).first
-		#@trave = Travel.find(62)
-		travel_cost = @travel.total_cost
-		json = Jbuilder.encode do |json|
-		  json.comparations PriceComparation.comparations() do |c|
-  		   json.id c.id
-  		   json.description c.description
-		   json.category c.category
-		   json.unit_cost c.unit_cost
-		   json.source_information c.source_information
-		   json.count (travel_cost / c.unit_cost).round()
-		  end		  
-		end
-		render json: json
-	  end
+module API
+  module V1
+    class PriceComparationController < ApplicationController
 
-	end
-   end
+      def compare
+        @travel = Travel.where(:id => params[:travel_id]).first
+        #@trave = Travel.find(62)
+        travel_cost = @travel.total_cost
+        json = Jbuilder.encode do |json|
+          json.comparations PriceComparation.comparations() do |c|
+            json.id c.id
+            json.description c.description
+            json.category c.category
+            json.unit_cost c.unit_cost
+            json.source_information c.source_information
+            json.count (travel_cost / c.unit_cost).round()
+          end
+        end
+        render json: json
+      end
+
+    end
+  end
 end
--- app/controllers/api/v1/vote_controller.rb
+++ app/controllers/api/v1/vote_controller.rb
@@ -1,43 +1,43 @@
-module Api
-    module V1
-	class VoteController < ApplicationController
-		def vote_up
-			@travel = Travel.where(:id => params[:travel_id]).first
-			travel_id = @travel.id
-			vote(travel_id,true)
-			render json: get_votes_json(travel_id)
-		end
-		
-		def vote_down
-			@travel = Travel.where(:id => params[:travel_id]).first
-			travel_id = @travel.id
-			vote(travel_id,false)
-			render json: get_votes_json(travel_id)
-		end
+module API
+  module V1
+    class VoteController < ApplicationController
+      def vote_up
+        @travel = Travel.where(:id => params[:travel_id]).first
+        travel_id = @travel.id
+        vote(travel_id, true)
+        render json: get_votes_json(travel_id)
+      end
 
-		private
+      def vote_down
+        @travel = Travel.where(:id => params[:travel_id]).first
+        travel_id = @travel.id
+        vote(travel_id, false)
+        render json: get_votes_json(travel_id)
+      end
 
-		def vote(travel_id,up)
-		    client_ip = request.remote_ip
-		    vote = Vote.where(:travel_id => travel_id, :ip => client_ip).first
-			if !vote
-			   vote = Vote.new
-			   vote.travel_id = travel_id
-			   vote.ip = client_ip
-			   vote.vote_down = !up
-			   vote.vote_up = up
-			   vote.save
-			end
-		    vote	
-		end
+      private
 
-		def get_votes_json(travel_id)
-		     Jbuilder.encode do |json|
-			  json.travel_id travel_id
-			  json.votes_up Vote.where(:vote_up =>true, :travel_id => travel_id).count
-			  json.votes_down Vote.where(:vote_down =>true, :travel_id => travel_id).count
-		     end	
-		end
-	end
-   end
+      def vote(travel_id, up)
+        client_ip = request.remote_ip
+        vote = Vote.where(:travel_id => travel_id, :ip => client_ip).first
+        if !vote
+          vote = Vote.new
+          vote.travel_id = travel_id
+          vote.ip = client_ip
+          vote.vote_down = !up
+          vote.vote_up = up
+          vote.save
+        end
+        vote
+      end
+
+      def get_votes_json(travel_id)
+        Jbuilder.encode do |json|
+          json.travel_id travel_id
+          json.votes_up Vote.where(:vote_up => true, :travel_id => travel_id).count
+          json.votes_down Vote.where(:vote_down => true, :travel_id => travel_id).count
+        end
+      end
+    end
+  end
 end
