66165fbfe7405d0a4c4cbbcdb41521f4bd52b0bb
Carlos Aguilar <caguilar@dwdandsolutions.com>
2014-08-24 17:08:47 -0600

    Adding constraint and path to remove the /api/v1/ path from controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-namespace :api do
-  namespace :v1 do
-	get '/price/compare/:travel_id', to: 'price_comparation#compare'
-	get '/vote/up/:travel_id', to: 'vote#vote_up'
-	get '/vote/down/:travel_id', to: 'vote#vote_down'
-	get '/travel/show/:id', to: 'travel#show'
-	get '/travel/recents', to: 'travel#travels_recents'
-	get '/travelers/institution/:id', to: 'travel#travelers_for_institucion'
+  namespace :api, path: '/', constraints: { subdomain: 'api' } do
+    namespace :v1 do
+    get '/price/compare/:travel_id', to: 'price_comparation#compare'
+    get '/vote/up/:travel_id', to: 'vote#vote_up'
+    get '/vote/down/:travel_id', to: 'vote#vote_down'
+    get '/travel/show/:id', to: 'travel#show'
+    get '/travel/recents', to: 'travel#travels_recents'
+    get '/travelers/institution/:id', to: 'travel#travelers_for_institucion'
+    end
   end
-end
 end
