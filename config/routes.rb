29428cad6d2e0137b0a6dd7ee541168436474ae2
Carlos Aguilar <caguilar@dwdandsolutions.com>
2014-08-25 23:22:42 -0600

    Adding a new way of routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,13 @@ Rails.application.routes.draw do
     get '/price/compare/:travel_id', to: 'price_comparation#compare'
     get '/vote/up/:travel_id', to: 'vote#vote_up'
     get '/vote/down/:travel_id', to: 'vote#vote_down'
-    get '/travel/show/:id', to: 'travel#show'
-    get '/travel/recents', to: 'travel#travels_recents'
+    with_options only: :show do |list_and_show|
+      list_and_show.resources :travels do
+        collection do
+          get 'recents'
+        end
+      end
+    end
     get '/travelers/institution/:id', to: 'travel#travelers_for_institucion'
     end
   end
