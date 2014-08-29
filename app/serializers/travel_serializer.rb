8c3be6734ca4f23dc0076001a18ef208b2f5c3b6
Carlos Aguilar <caguilar@dwdandsolutions.com>
2014-08-29 11:45:45 -0600

    Adding destination to serializer for travels

--- app/serializers/travel_serializer.rb
+++ app/serializers/travel_serializer.rb
@@ -1,5 +1,5 @@
 class TravelSerializer < ActiveModel::Serializer
-  attributes :id, :name, :passengers_number, :description, :total_cost
+  attributes :id, :name, :passengers_number, :description, :total_cost, :destination
 
   has_many :travelers
 end
