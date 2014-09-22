706153b4add12e32ef1adc73c3823ebb381997b3
Carlos Aguilar <caguilar@dwdandsolutions.com>
2014-09-21 21:44:41 -0600

    Adding travel start date and end date

--- app/serializers/travel_serializer.rb
+++ app/serializers/travel_serializer.rb
@@ -1,5 +1,5 @@
 class TravelSerializer < ActiveModel::Serializer
-  attributes :id, :name, :passengers_number, :description, :total_cost, :destination
+  attributes :id, :name, :passengers_number, :description, :total_cost, :destination, :start_date, :end_date
 
   has_many :travelers
 end
