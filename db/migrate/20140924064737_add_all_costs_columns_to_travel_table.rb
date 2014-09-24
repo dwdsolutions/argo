6586203028704beb526e5a2b60d0d8757b42b1b0
Carlos Aguilar <caguilar@dwdandsolutions.com>
2014-09-24 00:52:48 -0600

    Adding missing costs columns

---
+++ db/migrate/20140924064737_add_all_costs_columns_to_travel_table.rb
@@ -0,0 +1,8 @@
+class AddAllCostsColumnsToTravelTable < ActiveRecord::Migration
+  def change
+    add_column :travel, :travel_cost, :decimal
+    add_column :travel, :viatical_cost, :decimal
+    add_column :travel, :logement_cost, :decimal
+    add_column :travel, :other_cost, :string
+  end
+end
