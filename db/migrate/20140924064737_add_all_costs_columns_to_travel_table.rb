d4c59a8a12c2b3d007aa31787b198e117e4334dd
Carlos Aguilar <caguilar@dwdandsolutions.com>
2014-09-24 00:56:47 -0600

    Fixing field name

--- db/migrate/20140924064737_add_all_costs_columns_to_travel_table.rb
+++ db/migrate/20140924064737_add_all_costs_columns_to_travel_table.rb
@@ -2,7 +2,7 @@ class AddAllCostsColumnsToTravelTable < ActiveRecord::Migration
   def change
     add_column :travel, :travel_cost, :decimal
     add_column :travel, :viatical_cost, :decimal
-    add_column :travel, :logement_cost, :decimal
+    add_column :travel, :lodgement_cost, :decimal
     add_column :travel, :other_cost, :string
   end
 end
